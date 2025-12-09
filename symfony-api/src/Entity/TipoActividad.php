84b0e30e736f6037190966c23b4307f9331371fe
Alejacks <alejandrolopez2048@gmail.com>
2025-12-09 13:00:13 +0100

    Campos y getteres de TipoActividad añadidos

--- symfony-api/src/Entity/TipoActividad.php
+++ symfony-api/src/Entity/TipoActividad.php
@@ -3,18 +3,66 @@
 namespace App\Entity;
 
 use App\Repository\TipoActividadRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: TipoActividadRepository::class)]
+#[ORM\Table(name: 'TIPO_ACTIVIDAD')]
 class TipoActividad
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[ORM\Column(name: 'id_tipo_actividad', type: 'smallint')]
+    private ?int $idTipoActividad = null;
 
-    public function getId(): ?int
+    #[ORM\Column(name: 'descripcion', type: 'string', length: 20)]
+    private string $descripcion;
+
+    // Relación Inversa (ManyToMany) con Actividad, mapeada por 'tiposActividad' en Actividad.php
+    #[ORM\ManyToMany(targetEntity: Actividad::class, mappedBy: 'tiposActividad')]
+    private Collection $actividades;
+
+    #[ORM\ManyToMany(targetEntity: Voluntario::class, mappedBy: 'tiposActividad')]
+    private Collection $voluntarios;
+
+    public function __construct()
     {
-        return $this->id;
+        $this->actividades = new ArrayCollection();
+        $this->voluntarios = new ArrayCollection();
     }
+
+    public function getIdTipoActividad(): ?int
+    {
+        return $this->idTipoActividad;
+    }
+
+    public function getDescripcion(): string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Actividad>
+     */
+    public function getActividades(): Collection
+    {
+        return $this->actividades;
+    }
+
+    /**
+     * @return Collection<int, Voluntario>
+     */
+    public function getVoluntarios(): Collection
+    {
+        return $this->voluntarios;
+    }
+
 }
